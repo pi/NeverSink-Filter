2fe45c78cf4175b6204e58d92767a8d4ada809b8
Vif Ardente <vif.ardente@gmail.com>
2017-08-10 18:23:49 +0300

    cur fixes

--- mkfilt.bat
+++ mkfilt.bat
@@ -1,4 +1,3 @@
 @set poe_dir=%USERPROFILE%\Documents\My Games\Path of Exile
-@echo %poe_dir%
 @del "%poe_dir%\my_neversink*.filter"
 @for %%f in (neversink*.filter) do copy /b my-include.filter+"%%f" "%poe_dir%\my_%%f"
