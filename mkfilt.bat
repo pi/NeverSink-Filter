7c9545486779d757374a587d3496e5449adbd5f0
Vif Ardente <vif.ardente@gmail.com>
2017-08-18 02:28:52 +0300

    filter updates

--- mkfilt.bat
+++ mkfilt.bat
@@ -1,3 +1,3 @@
 @set poe_dir=%USERPROFILE%\Documents\My Games\Path of Exile
 @del "%poe_dir%\my_neversink*.filter"
-@for %%f in (neversink*.filter) do copy /b my-include.filter+"%%f" "%poe_dir%\my_%%f"
+@for %%f in (neversink*.filter) do @copy /b my-include.filter+"%%f" "%poe_dir%\my_%%f"
